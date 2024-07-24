94e636be8b227aa7e050a4e707abd3dd8b6d50e1
CesarLopz2 <cesareduardolopez325@gmail.com>
2024-07-24 07:59:30 -0600

    Primera version

---
+++ Conteo de palabras.py
@@ -0,0 +1,24 @@
+# Ingresar el texto con el que se trabajará
+texto = "El amor es una locura que ni el cura lo cura porque si el cura lo cura sería una locura del cura"
+
+# Separar el texto en palabras para poder realizar el conteo
+palabras = texto.split()
+
+# Crear un diccionario para almacenar los conteos
+conteo_palabras = {}
+
+# Definir algunas condiciones para cada palabra
+for palabra in palabras:
+    # Eliminar signos de puntuación al final de la palabra para un conteo preciso
+    palabra = palabra.strip(".,;:!?'")
+    # Convertir la palabra a minúsculas 
+    palabra = palabra.lower() 
+    # Realizar el conteo de las palabras    
+    if palabra in conteo_palabras:
+        conteo_palabras[palabra] += 1
+    else:
+        conteo_palabras[palabra] = 1
+
+# Imprimir el número de veces que cada palabra se repite
+for palabra, conteo in conteo_palabras.items():
+    print(f"'{palabra}' se repite {conteo} veces.")
